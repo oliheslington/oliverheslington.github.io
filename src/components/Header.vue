5fadfcd8b286b0e937ff46dd36d557014b7127ef
James Mitchell <hello@jamesmitchell.info>
2018-11-16 01:04:18 +0000

    Update icon pack, SVG logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,12 @@
 <template>
   <header>
     <router-link exact tag="svg" to="/projects" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="1000px" height="1000px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
-       <path d="M406.586,500c0.105,1.823,0.145,3.65,0.116,5.477c-0.638,41.243-34.588,74.161-75.831,73.523
-       	c-1.826,0.028-3.653-0.011-5.477-0.116c-41.179-2.388-72.625-37.705-70.237-78.884c-0.106-1.823-0.145-3.65-0.116-5.477
-       	c0.637-41.243,34.587-74.16,75.83-73.523c1.827-0.028,3.654,0.011,5.478,0.116C377.527,423.504,408.974,458.821,406.586,500z
-       	 M1000,500c0,276.143-223.857,500-500,500C223.858,1000,0,776.143,0,500S223.857,0,500,0S1000,223.857,1000,500z M503.543,500
-       	c0-96.972-75.215-163.215-172.672-163.215C233.415,336.785,158.2,403.015,158.2,500s75.214,163.214,172.671,163.214
-       	C428.328,663.214,503.543,596.972,503.543,500z M841.8,341.986h-95.571v111.656H633.172V341.986H537.6v315.542h95.572V536.9h113.057
-       	v120.628H841.8V341.986z"/>
+      <g>
+        <path class="outer" d="M500,0c276.143,0,500,223.857,500,500s-223.857,500-500,500S0,776.143,0,500S223.857,0,500,0z"/>
+        <path class="inner" d="M330.871,336.785C233.415,336.785,158.2,403.015,158.2,500s75.214,163.214,172.671,163.214 S503.543,596.972,503.543,500S428.328,336.785,330.871,336.785z"/>
+        <path class="inner" d="M841.8,657.528h-95.57V536.9H633.172v120.628H537.6V341.986h95.572v111.655h113.058V341.986h95.57V657.528z"/>
+        <path class="outer" d="M336.349,421.116c-1.824-0.105-3.651-0.146-5.478-0.116c-41.243-0.637-75.193,32.28-75.83,73.523 c-0.029,1.825,0.01,3.652,0.116,5.477c-2.388,41.179,29.058,76.496,70.237,78.884c1.824,0.105,3.651,0.146,5.477,0.116 c41.243,0.638,75.192-32.28,75.831-73.523c0.029-1.825-0.011-3.652-0.116-5.477C408.974,458.821,377.527,423.504,336.349,421.116z"/>
+      </g>
     </router-link>
     <nav>
       <router-link tag="a" class="caps" to="/projects">Projects</router-link>
@@ -25,23 +24,44 @@
       padding: 2rem 2rem 0;
 
       svg {
-          border-radius: 100%;
           cursor: pointer;
-          fill: $color-dark;
-          height: 70px;
+          height: 3.5rem;
           transform: translateX(-.1rem);
           transition: transform 1s cubic-bezier(.19, 1, .22, 1);
           width: auto;
-
-          @include small {
-              height: 56px;
-          }
 
           &:hover {
               transform: translateX(-.1rem) scale(1.075);
 
               &.router-link-exact-active {
                   transform: translateX(-.1rem);
+              }
+          }
+
+          .outer {
+              fill: $color-dark;
+
+              .inverted & {
+                  fill: $color-light;
+              }
+          }
+
+          .inner {
+              fill: $color-light;
+
+              .inverted & {
+                  fill: $color-dark;
+              }
+          }
+      }
+
+      &.inverted {
+          svg {
+              .outer {
+                  fill: $color-light;
+              }
+              .inner {
+                  fill: $color-dark;
               }
           }
       }
@@ -69,20 +89,8 @@
           }
       }
 
-      &.inverted,
-      .inverted & {
-          svg {
-              background: $color-dark;
-              fill: $color-light;
-          }
-      }
-
-      &.inverted {
-          background: 0;
-
-          nav a::after {
-              background: $color-light;
-          }
+      &.inverted nav a::after {
+          background: $color-light;
       }
   }
 </style>
